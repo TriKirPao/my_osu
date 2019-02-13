ac9a43205d05bb7e2811575a0a7cdad00b7ae6f0
kirian <kirian.senaux@epitech.eu>
2019-02-13 14:42:59 +0100

    beforing

--- menu2.py
+++ menu2.py
@@ -72,8 +72,7 @@ def second_screen(play):
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == 27:
-                    pygame.quit()
-                    exit()
+                    break
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 4 and start > 0:
                     start -= 1
@@ -91,30 +90,4 @@ def second_screen(play):
                 i += 1
             j += 1
         pygame.display.flip()
-
-""" import os
-import glob
-import pygame
-pygame.init()
-ecran = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
-list = []
-for root, dirs, files in os.walk("song", topdown=False):
-    for name in dirs:
-        for filename in glob.glob(os.path.join(os.path.join(root, name), '*.mp3')):
-            list.append(filename)
-print("c'est la liste : {}".format(list))
-
-pygame.mixer.music.load("/home/jonathan/Bureau/musiquegroove/pokemon/pokemon-b2w2-remix-champion-iris-battle-music-mashup-hq.mp3")
-pygame.mixer.music.set_volume(0.25)
-pygame.mixer.music.play()
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_f:
-            pygame.mixer.music.rewind()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-            pygame.mixer.music.pause()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_z:
-            pygame.mixer.music.unpause()
- """
+    return
