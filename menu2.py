cfeb39827f33685c959f02b4906672d32a3587ff
Me <tristan.merigny@epitech.eu>
2019-02-13 12:35:30 +0100

    Adding textures

--- menu2.py
+++ menu2.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
 import pygame
+import os
 
 pygame.init()
 window_resolution = (1920, 1080)
-blue = [0, 60, 155]
-black = [0, 0, 0]
-white = [255, 255, 255]
+black = [255, 255, 255]
 
 pygame.display.set_caption("Rectangle test")
 window_surface = pygame.display.set_mode(window_resolution, pygame.FULLSCREEN)
@@ -17,11 +16,11 @@ window_surface = pygame.display.set_mode(window_resolution, pygame.FULLSCREEN)
 class rectangle:
 
     def __init__(self, y):
-        self.x = 1220
+        self.x = 1000
         self._y = y
         self.width = 700
         self.height = 135
-        self.rec = 0
+        self.img = 0
 
     def y_getage(self):
         return self._y
@@ -35,8 +34,8 @@ class rectangle:
     y = property(y_getage, y_setage)
 
     def rect_creation(self):
-        self.rec = pygame.Rect(self.x, self.y, self.width, self.height)
-        return (self.rec)
+        self.img = pygame.image.load("texture/selection.png")
+        return (self.img)
 
 
 my_rec = []
@@ -45,14 +44,19 @@ for i in range(0, 8):
 
 def text_creation(music):
     arial_font = pygame.font.SysFont('police/police.ttf', 60, True)
-    music_name = arial_font.render(music, True, white)
+    color = (255, 255, 255, 128)
+    music_name = arial_font.render(music, True, color)
     return (music_name)
 
-music = ["Yes", "No", "Yah", "Hey", "Bah", "Hello", "zfz", "fzfz", "ve", "ffzs", "fefe"]
+music = ["Yes", "No", "Yah", "Hey"]
 
 def list_creation(music, start):
     m_list = []
-    for i in range(start, start + 8):
+    if (len(music) < 8):
+        size = len(music)
+    else:
+        size = 8
+    for i in range(start, start + size):
         m_list.append(music[i])
     return (m_list)
 
@@ -60,21 +64,27 @@ def second_screen():
     launched = True
     start = 0
     while launched:
+        os.system('clear')
+        screen = pygame.image.load("pic/1.jpg")
+        window_surface.blit(screen, [0, 0])
         for event in pygame.event.get():
-            window_surface.fill((black))
+            if event.type == pygame.KEYDOWN:
+                if event.key == 27:
+                    pygame.quit()
+                    exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 4 and start > 0:
                     start -= 1
                 elif event.button == 5 and start < len(music) - 8:
                     start += 1
-            music_list = list_creation(music, start)
             if event.type == pygame.QUIT:
-                launched = False
-            i = 0
-            for rec in my_rec:
-                pygame.draw.rect(window_surface, blue, rec.rect_creation(), 2)
-                window_surface.blit(text_creation(music_list[i]), [rec.x + 40, rec.y + rec.height / 2 - 30])
-                i += 1
-            pygame.display.flip()
+                    launched = False
+        music_list = list_creation(music, start)
+        i = 0
+        for rec in my_rec:
+            window_surface.blit(my_rec[i].rect_creation(), [my_rec[i].x, my_rec[i].y])
+            window_surface.blit(text_creation(music_list[i]), [rec.x + 85, rec.y + rec.height / 2 - 15])
+            i += 1
+        pygame.display.flip()
 
 second_screen()
