8a73791688a7c0e3096ac791be53bed777f3a0a9
Sirocco <paolo.reant-hamadi@epitech.eu>
2019-02-15 08:52:39 +0100

    semi collision added

--- game.py
+++ game.py
@@ -5,9 +5,14 @@ import math
 def check_click(event, circle_list, play):
     x,y = pygame.mouse.get_pos()
     for i in range(0, len(circle_list)):
-        if circle_list[i].bool == 1 and math.pow((x - int(circle_list[i].x) - 40), 2) + math.pow((y - int(circle_list[i].y) - 40), 2) <= math.pow((64), 2) and play.number == circle_list[i].number:
+        print("nbr en cours : {}, number qui apparait: {}, booleen du cercle: {}, valeur de la gauche {}, valeur de la droite :{}".format(play.number, circle_list[i].number, circle_list[i].bool, math.pow((x - int(circle_list[i].x) - 40), 2) + math.pow((y - int(circle_list[i].y) - 40), 2), math.pow((1200), 2)))
+        if int(circle_list[i].bool) == 1 and math.pow((x - int(circle_list[i].x) - 40), 2) + math.pow((y - int(circle_list[i].y) - 40), 2) <= math.pow((50), 2) and play.number == int(circle_list[i].number):
+            print("salut")
             circle_list[i].bool = 0
-            play.number = circle_list[i + 1].number
+            try:
+                play.number = circle_list[i + 1].number
+            except:
+                pass
 
 def load_pause_menu(play, circle_list):
     for mainevent in pygame.event.get():
@@ -50,7 +55,7 @@ def load_game(play, name):
         time += clock.get_time()
         screen.blit(timage, (0, 0))
         event = load_pause_menu(play, circle_list)
-        if event == 2:
+        if event == 2 or pygame.mixer.music.get_busy() == False:
             launch = False
         for circle in circle_list:
             if circle.time * 1000 <= time and circle.bool == 2:
@@ -63,3 +68,4 @@ def load_game(play, name):
                 screen.blit(circlepic, (int(circle.x), int(circle.y)))
                 print_number(screen, circle)
         pygame.display.flip()
+    play.number = 0
